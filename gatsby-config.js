5444da2eb3848329be2db7e721c4bc8ca012cd0e
Matthieu Caron <contact@matthieucaron.fr>
2021-01-12 21:55:42 +0100

    add: prismic source

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,23 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-prismic`,
+      options: {
+        repositoryName: `test-synapse`,
+        accessToken: `${process.env.API_KEY}`,
+        linkResolver: ({ node, key, value }) => post => `/${post.uid}`,
+        schemas: {
+          homepage: require("./src/schemas/homepage.json"),
+        },
+      },
+    },
+  ],
 }
