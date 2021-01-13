b26a88d12acd885333776b3b1e6b4b35d53398d9
Matthieu Caron <contact@matthieucaron.fr>
2021-01-13 13:44:38 +0100

    add: config prismic & linter

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,23 +4,24 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+require('dotenv').config({
+	path: `.env.${process.env.NODE_ENV}`,
+});
 
 module.exports = {
-  /* Your site config here */
-  plugins: [
-    {
-      resolve: `gatsby-source-prismic`,
-      options: {
-        repositoryName: `test-synapse`,
-        accessToken: `${process.env.API_KEY}`,
-        linkResolver: ({ node, key, value }) => post => `/${post.uid}`,
-        schemas: {
-          homepage: require("./src/schemas/homepage.json"),
-        },
-      },
-    },
-  ],
-}
+	/* Your site config here */
+	plugins: [
+		'gatsby-plugin-eslint',
+		{
+			resolve: `gatsby-source-prismic`,
+			options: {
+				repositoryName: `test-synapse`,
+				accessToken: `${process.env.API_KEY}`,
+				linkResolver: ({ node, key, value }) => (post) => `/${post.uid}`,
+				schemas: {
+					homepage: require('./src/schemas/homepage.json'),
+				},
+			},
+		},
+	],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,34 @@
-import React from "react"
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Home({ data: { prismicHomepage } }) {
+	const prismicContent = prismicHomepage.data;
+	if (!prismicContent) return null;
+
+	return <div>{prismicContent.title.text}</div>;
 }
+
+export const homeQuery = graphql`
+	query HomeQuery {
+		prismicHomepage {
+			data {
+				title {
+					text
+				}
+			}
+		}
+	}
+`;
+
+Home.propTypes = {
+	data: PropTypes.shape({
+		prismicHomepage: PropTypes.shape({
+			data: PropTypes.shape({
+				title: PropTypes.shape({
+					text: PropTypes.string.isRequired,
+				}),
+			}),
+		}),
+	}).isRequired,
+};
