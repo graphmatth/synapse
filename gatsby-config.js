cdbb091837f82050a2e90a18feb0bebb3f57820a
matthieucaron64 <contact@matthieucaron.fr>
2021-01-24 00:41:25 +0100

    fix:  google analytics plugin config

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,6 +39,9 @@ module.exports = {
 			options: {
 				// The property ID; the tracking code won't be generated without it
 				trackingId: process.env.GA_TRACKING_ID,
+				head: false,
+				pageTransitionDelay: 0,
+				defer: false,
 			},
 		},
 		{
